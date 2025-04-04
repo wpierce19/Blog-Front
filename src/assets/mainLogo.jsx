a42ed2d9e8e8c67e36c0cb48ed91fbb2c2331df6
wpierce19 <126428398+wpierce19@users.noreply.github.com>
2025-04-03 21:49:52 -0600

    Update mainLogo.jsx
    removed style from logo

--- src/assets/mainLogo.jsx
+++ src/assets/mainLogo.jsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function MainLogo() {
     return (
         <svg
-            style={{position: "absolute", left: "0", top: "0"}} 
             xmlns="http://www.w3.org/2000/svg" 
             xmlns:xlink="http://www.w3.org/1999/xlink"
             viewBox="0 0 375 374.999991" 
